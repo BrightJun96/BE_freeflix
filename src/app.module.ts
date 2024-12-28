e46804f5c60963ad34bdfca85e28b814f33a11ba
jjalseu <cjfwns966@gmail.com>
2024-12-29 04:01:59 +0900

    - DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { MovieModule } from "./movie/movie.module";
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "jjalseu",
+      password: "love9785@@",
+      database: "mydatabase",
+      entities: [],
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
