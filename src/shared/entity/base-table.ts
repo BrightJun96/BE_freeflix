7830bf6d9a9d106a2fce692d62210c0ef5c97835
jjalseu <cjfwns966@gmail.com>
2025-01-05 03:08:19 +0900

    exclude : base-table-entity에 적용

--- src/shared/entity/base-table.ts
+++ src/shared/entity/base-table.ts
@@ -1,13 +1,17 @@
+import { Exclude } from "class-transformer";
 import { CreateDateColumn, VersionColumn } from "typeorm";
 
 // 모든 엔티티에 공통적으로 사용되는 필드를 BaseEntity 클래스에 정의
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date;
 
   @CreateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
