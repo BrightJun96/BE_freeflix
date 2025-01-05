9e37d4cabd2bfe648f0d591a2dbb6615e2228db9
jjalseu <cjfwns966@gmail.com>
2025-01-05 14:46:46 +0900

    user : user 모듈 생성

---
+++ src/user/user.service.ts
@@ -0,0 +1,56 @@
+import {
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entities/user.entity";
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    return this.userRepository.save(createUserDto);
+  }
+
+  async findAll() {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        "존재하지 않는 사용자입니다.",
+      );
+    }
+
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.findOne(id);
+
+    await this.userRepository.update(
+      user.id,
+      updateUserDto,
+    );
+
+    return await this.findOne(id);
+  }
+
+  async remove(id: number) {
+    await this.userRepository.delete(id);
+    return id;
+  }
+}
