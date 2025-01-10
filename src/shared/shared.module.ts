8e740e1152efcb113adcbe5ee7bf38efd5cb27ee
jjalseu <cjfwns966@gmail.com>
2025-01-10 23:28:12 +0900

    feature : task-schduling - 임시 파일 하루마다 삭제 - 좋아요,싫어요 숫자 한시간마다 일괄 업데이트

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Movie } from "../movie/entities/movie.entity";
 import { SharedService } from "./shared.service";
 import { TasksService } from "./tasks.service";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Movie])],
   controllers: [],
   providers: [SharedService, TasksService],
   exports: [SharedService],
--- src/shared/tasks.service.ts
+++ src/shared/tasks.service.ts
@@ -1,12 +1,61 @@
 import { Injectable } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
+import { InjectRepository } from "@nestjs/typeorm";
+import { readdir, unlink } from "node:fs/promises";
+import { join, parse } from "path";
+import { Repository } from "typeorm";
+import { Movie } from "../movie/entities/movie.entity";
 
 @Injectable()
 export class TasksService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>,
+  ) {}
 
-  @Cron("* * * * * *")
-  taskScheduler() {
-    console.log("1초마다 실행되는 스케줄러입니다.");
+  // 하루마다 temp 폴더에 있는 파일 삭제
+  @Cron("* * 0 * * *")
+  async eraseOrphanFiles() {
+    // 1.temp 파일을 읽어오셈
+    // 2.파일들뒤에 붙어있는 시간가져오셈.
+    // 3.가져오 시간과 현재시간과 비교해서 24시간이 지났으면 삭제(Promise.all)
+
+    const files = await readdir(
+      join(process.cwd(), "public", "temp"),
+    );
+
+    const deleteFiles = files.filter((file) => {
+      const fileName = parse(file).name;
+
+      const createdFileDate = parseInt(
+        fileName.split("-").at(-1),
+      );
+
+      const aDayInMills = 24 * 60 * 60 * 1000;
+
+      return Date.now() - createdFileDate > aDayInMills;
+    });
+
+    await Promise.all(
+      deleteFiles.map((file) =>
+        unlink(join(process.cwd(), "public", "temp", file)),
+      ),
+    );
+  }
+
+  // 1시간마다 좋아요, 싫어요 갱신
+  @Cron("* 0 * * * *")
+  async updateMovieLikes() {
+    await this.movieRepository.query(
+      `
+          update movie m set "dislikeCount" = (select count(*) from movie_user_like mul where m.id = mul."movieId" and mul."isLike"=false )
+          `,
+    );
+
+    await this.movieRepository.query(
+      `
+          update movie m set "likeCount" = (select count(*) from movie_user_like mul where m.id = mul."movieId" and mul."isLike"=true )
+          `,
+    );
   }
 }
