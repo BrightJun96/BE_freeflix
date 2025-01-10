e81aeeee5b21f32fa7cc4b4ad74b2b77f9507641
jjalseu <cjfwns966@gmail.com>
2025-01-11 01:17:38 +0900

    doc : 스웨거 문서 셋팅

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import {
+  DocumentBuilder,
+  SwaggerModule,
+} from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("freeflix API")
+    .setDescription("freeflix API 문서")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(
+    app,
+    config,
+  );
+
+  SwaggerModule.setup("doc", app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // 요청에 없는 속성은 제거
