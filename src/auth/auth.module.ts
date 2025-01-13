8163f3fa532ce37b63e967187d4f240d2622f89c
jjalseu <cjfwns966@gmail.com>
2025-01-13 20:39:34 +0900

    test : 유저 모듈 > 테스트 코드 작성

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../user/entities/user.entity";
+import { UserModule } from "../user/user.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtStrategy } from "./strategy/jwt.strategy";
@@ -11,6 +12,7 @@ import { LocalStrategy } from "./strategy/local.strategy";
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({}),
+    UserModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
