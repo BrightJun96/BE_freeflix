1c91f49b9cf2a6455d45d91cad89cc4a09e81c2b
jjalseu <cjfwns966@gmail.com>
2025-01-14 11:51:54 +0900

    test : auth service > 테스트 코드 작성 완료 !!

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -7,9 +7,10 @@ import { ConfigService } from "@nestjs/config";
 import { JwtService } from "@nestjs/jwt";
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
-import { afterEach, describe } from "node:test";
+import * as bcrypt from "bcrypt";
+import { describe } from "node:test";
 import { Repository } from "typeorm";
-import { User } from "../user/entities/user.entity";
+import { Role, User } from "../user/entities/user.entity";
 import { UserService } from "../user/user.service";
 import { AuthService } from "./auth.service";
 
@@ -36,8 +37,8 @@ const mockCacheManager = {
 
 describe("AuthService", () => {
   let authService: AuthService;
-  let userService: UserService;
   let userRepository: Repository<User>;
+  let userService: UserService;
   let configService: ConfigService;
   let jwtService: JwtService;
   let cacheManager: Cache;
@@ -93,6 +94,7 @@ describe("AuthService", () => {
   afterEach(() => {
     jest.clearAllMocks();
   });
+
   describe("tokenBlock", () => {
     it("should block a token", async () => {
       const token = "token";
@@ -152,6 +154,249 @@ describe("AuthService", () => {
 
       expect(() =>
         authService.parseBasicToken(rawToken),
+      ).toThrow(BadRequestException);
+    });
+  });
+
+  describe("register", () => {
+    it("should register a user by passing email and password parsed from token", async () => {
+      const rawToken =
+        "Basic dGVzdEBjb2RlZmFjdG9yeS5haTp0ZXN0dGVzdA==";
+
+      const user = {
+        email: "test@codefactory.ai",
+        password: "testtest",
+      };
+
+      jest
+        .spyOn(authService, "parseBasicToken")
+        .mockReturnValue(user);
+
+      const result = authService.parseBasicToken(rawToken);
+
+      expect(result).toEqual(user);
+      expect(
+        authService.parseBasicToken,
+      ).toHaveBeenCalledWith(rawToken);
+
+      jest
+        .spyOn(userService, "create")
+        .mockResolvedValue(user as User);
+
+      const createdUser = await userService.create(
+        result as User,
+      );
+
+      expect(userService.create).toHaveBeenCalledWith(
+        result,
+      );
+
+      const registerResult =
+        await authService.register(rawToken);
+
+      expect(registerResult).toEqual(createdUser);
+    });
+  });
+
+  describe("authenticate", () => {
+    it("should authenticate user", async () => {
+      const user = {
+        email: "test@codefactory.ai",
+        password: "testtest",
+      };
+
+      jest
+        .spyOn(userRepository, "findOne")
+        .mockResolvedValue(user as User);
+
+      const userFromRepo = await userRepository.findOne({
+        where: {
+          email: user.email,
+        },
+      });
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          email: user.email,
+        },
+      });
+
+      expect(userFromRepo).toEqual(user);
+
+      jest
+        .spyOn(bcrypt, "compare")
+        .mockImplementation((pw, userPw) => true);
+
+      await bcrypt.compare(
+        user.password,
+        userFromRepo.password,
+      );
+
+      expect(bcrypt.compare).toHaveBeenCalledWith(
+        user.password,
+        userFromRepo.password,
+      );
+
+      const result = await authService.authenticate(
+        user.email,
+        user.password,
+      );
+
+      expect(result).toEqual(user as User);
+    });
+
+    it("should throw an error if user is not found", async () => {
+      const userInfo = {
+        email: "test@codefactory.ai",
+        password: "testtest",
+      };
+
+      jest
+        .spyOn(userRepository, "findOne")
+        .mockResolvedValue(null);
+
+      await expect(
+        authService.authenticate(
+          userInfo.email,
+          userInfo.password,
+        ),
+      ).rejects.toThrow(BadRequestException);
+    });
+
+    it("should throw an error if password does not match", async () => {
+      const userInfo = {
+        email: "test@codefactory.ai",
+        password: "testtest",
+      };
+
+      jest
+        .spyOn(userRepository, "findOne")
+        .mockResolvedValue(userInfo as User);
+
+      jest
+        .spyOn(bcrypt, "compare")
+        .mockImplementation((repoPW, userPW) => false);
+
+      await expect(
+        authService.authenticate(
+          userInfo.email,
+          userInfo.password,
+        ),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe("issueToken", () => {
+    const user = {
+      id: 1,
+      role: Role.admin,
+    };
+
+    const token = "Sdfsdfvsdfgsdfgsdfgdf";
+    beforeEach(() => {
+      jest
+        .spyOn(configService, "get")
+        .mockReturnValue("secret");
+
+      jest
+        .spyOn(jwtService, "signAsync")
+        .mockResolvedValue(token);
+    });
+
+    it("should issue access token through user Info", async () => {
+      const result = await authService.issueToken(
+        user,
+        false,
+      );
+
+      expect(jwtService.signAsync).toHaveBeenCalledWith(
+        {
+          sub: 1,
+          role: Role.admin,
+          type: "access",
+        },
+        {
+          secret: "secret",
+          expiresIn: 300,
+        },
+      );
+
+      expect(result).toEqual(token);
+    });
+
+    it("should issue refresh token through user Info", async () => {
+      const result = await authService.issueToken(
+        user,
+        true,
+      );
+
+      expect(jwtService.signAsync).toHaveBeenCalledWith(
+        {
+          sub: 1,
+          role: Role.admin,
+          type: "refresh",
+        },
+        {
+          secret: "secret",
+          expiresIn: "24h",
+        },
+      );
+
+      expect(result).toEqual(token);
+    });
+  });
+
+  describe("login", () => {
+    it("should login through rawToken", async () => {
+      const rawToken = "Basic sdfsdf";
+
+      const authInfo = {
+        email: "test@codefactory.ai",
+        password: "1234",
+      };
+
+      const user = {
+        id: 1,
+        role: Role.admin,
+      };
+
+      const mockedToken = "mockToken";
+      jest
+        .spyOn(authService, "parseBasicToken")
+        .mockReturnValue(authInfo);
+      jest
+        .spyOn(authService, "authenticate")
+        .mockResolvedValue(user as User);
+
+      jest
+        .spyOn(authService, "issueToken")
+        .mockResolvedValue("mockToken");
+
+      const result = await authService.login(rawToken);
+
+      expect(
+        authService.parseBasicToken,
+      ).toHaveBeenCalledWith(rawToken);
+
+      expect(authService.authenticate).toHaveBeenCalledWith(
+        authInfo.email,
+        authInfo.password,
+      );
+      expect(authService.issueToken).toHaveBeenCalledTimes(
+        2,
+      );
+
+      expect(result).toEqual({
+        accessToken: mockedToken,
+        refreshToken: mockedToken,
+      });
+    });
+  });
+
+  describe("validateRefreshToken", () => {
+    it("should throw an error if token is not refreshToken", () => {
+      expect(() =>
+        authService.validateRefreshToken("access"),
       ).toThrow(BadRequestException);
     });
   });
