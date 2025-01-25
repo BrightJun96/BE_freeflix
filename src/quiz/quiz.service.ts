c2ebe93977c87efed40c3ff36a040ac059b13147
jjalseu <cjfwns966@gmail.com>
2025-01-25 18:04:51 +0900

    퀴즈 수정 service 메서드 분리

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -10,6 +10,8 @@ import { CheckAnswerDto } from "./dto/check-answer.dto";
 import { CreateMultipleChoiceDto } from "./dto/create-multiple-choice.dto";
 import { CreateQuizMetaDataDto } from "./dto/create-quiz-meta-data.dto";
 import { CreateQuizDto } from "./dto/create-quiz.dto";
+import { UpdateMultipleChoiceDto } from "./dto/update-multiple-choice.dto";
+import { UpdateQuizMetaDataDto } from "./dto/update-quiz-meta-data.dto";
 import { UpdateQuizDto } from "./dto/update-quiz.dto";
 import { MultipleChoice } from "./entities/multiple-choice.entity";
 import { QuizMetaData } from "./entities/quiz-meta-data.entity";
@@ -215,48 +217,96 @@ export class QuizService {
     const quiz = await this.findOneById(id);
 
     if (updateQuizDto.quizMetaData) {
-      await qr.manager
-        .createQueryBuilder()
-        .update(QuizMetaData)
-        .set(updateQuizDto.quizMetaData)
-        .where("id=:id", {
-          id: quiz.quizMetaData.id,
-        })
-        .execute();
+      await this.updateMetaData(
+        quiz.quizMetaData.id,
+        updateQuizDto.quizMetaData,
+        qr,
+      );
     }
 
     if (updateQuizDto.multipleChoices) {
-      const ids = updateQuizDto.multipleChoices.map(
-        (choice) => choice.id,
-      ); // 업데이트할 ID 추출
-
-      const contentCase = updateQuizDto.multipleChoices
-        .map((choice) =>
-          choice.content
-            ? `WHEN id = ${choice.id} THEN '${choice.content}'`
-            : "",
-        )
-        .join(" ");
-      const orderCase = updateQuizDto.multipleChoices
-        .map((choice) =>
-          choice.order
-            ? `WHEN id = ${choice.id} THEN '${choice.order}'`
-            : "",
-        )
-        .join(" ");
-
-      await qr.manager
-        .createQueryBuilder()
-        .update(MultipleChoice)
-        .set({
-          content: () =>
-            `CASE ${contentCase} ELSE content END`,
-          order: () => `CASE ${orderCase} ELSE "order" END`,
-        })
-        .where("id IN (:...ids)", { ids })
-        .execute();
+      await this.updateMultipleChoice(
+        updateQuizDto.multipleChoices,
+        qr,
+      );
     }
 
+    await this.updateQuiz(updateQuizDto, quiz.id, qr);
+
+    return qr.manager.findOne(Quiz, {
+      relations: [
+        Relations.QUIZ.META,
+        Relations.QUIZ.MULTIPLE,
+      ],
+      where: {
+        id,
+      },
+    });
+  }
+
+  /**
+   * 메타 데이터 수정
+   */
+
+  async updateMetaData(
+    metaId: number,
+    updateQuizMetaDataDto: UpdateQuizMetaDataDto,
+    qr: QueryRunner,
+  ) {
+    await qr.manager
+      .createQueryBuilder()
+      .update(QuizMetaData)
+      .set(updateQuizMetaDataDto)
+      .where("id=:id", {
+        id: metaId,
+      })
+      .execute();
+  }
+
+  /**
+   * 객관식 답안 수정
+   */
+  async updateMultipleChoice(
+    multipleChoices: UpdateMultipleChoiceDto[],
+    qr: QueryRunner,
+  ) {
+    const ids = multipleChoices.map((choice) => choice.id); // 업데이트할 ID 추출
+
+    const contentCase = multipleChoices
+      .map((choice) =>
+        choice.content
+          ? `WHEN id = ${choice.id} THEN '${choice.content}'`
+          : "",
+      )
+      .join(" ");
+    const orderCase = multipleChoices
+      .map((choice) =>
+        choice.order
+          ? `WHEN id = ${choice.id} THEN '${choice.order}'`
+          : "",
+      )
+      .join(" ");
+
+    await qr.manager
+      .createQueryBuilder()
+      .update(MultipleChoice)
+      .set({
+        content: () =>
+          `CASE ${contentCase} ELSE content END`,
+        order: () => `CASE ${orderCase} ELSE "order" END`,
+      })
+      .where("id IN (:...ids)", { ids })
+      .execute();
+  }
+
+  /**
+   * 퀴즈 수정
+   */
+  async updateQuiz(
+    updateQuizDto: UpdateQuizDto,
+    quizId: number,
+    qr: QueryRunner,
+  ) {
     await qr.manager
       .createQueryBuilder()
       .update(Quiz)
@@ -269,20 +319,9 @@ export class QuizService {
         answer: updateQuizDto.answer,
       })
       .where("id=:id", {
-        id,
+        id: quizId,
       })
-
       .execute();
-
-    return qr.manager.findOne(Quiz, {
-      relations: [
-        Relations.QUIZ.META,
-        Relations.QUIZ.MULTIPLE,
-      ],
-      where: {
-        id,
-      },
-    });
   }
 
   /**
