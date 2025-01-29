ee4884e033579738ebb2c2b020bad435fcb8ec04
jjalseu <cjfwns966@gmail.com>
2025-01-29 16:49:01 +0900

    영화 API 주석

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,34 +1,9 @@
 import {
-  Body,
   ClassSerializerInterceptor,
   Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Query,
-  Request,
   UseInterceptors,
 } from "@nestjs/common";
-import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiTags,
-} from "@nestjs/swagger";
-import { QueryRunner as QR } from "typeorm";
-import { Public } from "../auth/decorator/public.decorator";
-import { RBAC } from "../auth/decorator/rbac.decorator";
-import { QueryRunner } from "../shared/decorator/query-runner.decorator";
-import { Throttle } from "../shared/decorator/throttle.decorator";
-import { TransactionInterceptor } from "../shared/interceptor/transaction.interceptor";
-import { PositiveIntPipe } from "../shared/pipe/positive-int.pipe";
-import { UserId } from "../user/decorator/user-id.decorator";
-import { Role } from "../user/entities/user.entity";
-import { CreateMovieDto } from "./dto/create-movie.dto";
-import { GetMovieDto } from "./dto/get-movie.dto";
-import { UpdateMovieDto } from "./dto/update-movie.dto";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { MovieService } from "./movie.service";
 
 @Controller("movie")
@@ -40,106 +15,106 @@ export class MovieController {
     private readonly movieService: MovieService,
   ) {}
 
-  @Public()
-  @Get()
-  @Throttle({
-    count: 5,
-    unit: "minute",
-  })
-  getMovies(
-    @Query()
-    getMovieDto: GetMovieDto,
-    @UserId() userId?: number,
-  ) {
-    return this.movieService.findAll(getMovieDto, userId);
-  }
-
-  @Public()
-  @Get("recent")
-  @ApiOperation({
-    description: "최신 영화 조회 API",
-  })
-  getRecentMovies() {
-    return this.movieService.findLatestMovies();
-  }
-
-  @Public()
-  @Get(":id")
-  getMovie(
-    @Param("id", ParseIntPipe, PositiveIntPipe)
-    id: number,
-    @Request() req: any,
-  ) {
-    const session = req.session;
-
-    const movieCount = session.movieCount ?? {};
-
-    req.session.movieCount = {
-      ...movieCount,
-      [id]: movieCount[id] ? movieCount[id] + 1 : 1,
-    };
-    return this.movieService.findOne(id);
-  }
-
-  @Post()
-  @RBAC(Role.admin)
-  @UseInterceptors(TransactionInterceptor)
-  postMovie(
-    @Body() createMovieDto: CreateMovieDto,
-    @QueryRunner() qr: QR,
-    @UserId() userId: number,
-  ) {
-    return this.movieService.create(
-      createMovieDto,
-      userId,
-      qr,
-    );
-  }
-
-  @Patch(":id")
-  @RBAC(Role.admin)
-  @UseInterceptors(TransactionInterceptor)
-  patchMovie(
-    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-    @Body() updateMovieDto: UpdateMovieDto,
-    @QueryRunner() qr: QR,
-  ) {
-    return this.movieService.update(id, updateMovieDto, qr);
-  }
-
-  @Delete(":id")
-  @RBAC(Role.admin)
-  @UseInterceptors(TransactionInterceptor)
-  deleteMovie(
-    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-    @QueryRunner() qr: QR,
-  ) {
-    return this.movieService.remove(id, qr);
-  }
-
-  // 좋아요
-  @Post(":id/like")
-  @ApiOperation({
-    description: "좋아요 API",
-  })
-  likeMovie(
-    @Param("id", ParseIntPipe, PositiveIntPipe)
-    movieId: number,
-    @UserId() userId: number,
-  ) {
-    return this.movieService.likeMovie(movieId, userId);
-  }
-
-  // 싫어요
-  @Post(":id/dislike")
-  @ApiOperation({
-    description: "싫어요 API",
-  })
-  dislikeMovie(
-    @Param("id", ParseIntPipe, PositiveIntPipe)
-    movieId: number,
-    @UserId() userId: number,
-  ) {
-    return this.movieService.dislikeMovie(movieId, userId);
-  }
+  // @Public()
+  // @Get()
+  // @Throttle({
+  //   count: 5,
+  //   unit: "minute",
+  // })
+  // getMovies(
+  //   @Query()
+  //   getMovieDto: GetMovieDto,
+  //   @UserId() userId?: number,
+  // ) {
+  //   return this.movieService.findAll(getMovieDto, userId);
+  // }
+  //
+  // @Public()
+  // @Get("recent")
+  // @ApiOperation({
+  //   description: "최신 영화 조회 API",
+  // })
+  // getRecentMovies() {
+  //   return this.movieService.findLatestMovies();
+  // }
+  //
+  // @Public()
+  // @Get(":id")
+  // getMovie(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe)
+  //   id: number,
+  //   @Request() req: any,
+  // ) {
+  //   const session = req.session;
+  //
+  //   const movieCount = session.movieCount ?? {};
+  //
+  //   req.session.movieCount = {
+  //     ...movieCount,
+  //     [id]: movieCount[id] ? movieCount[id] + 1 : 1,
+  //   };
+  //   return this.movieService.findOne(id);
+  // }
+  //
+  // @Post()
+  // @RBAC(Role.admin)
+  // @UseInterceptors(TransactionInterceptor)
+  // postMovie(
+  //   @Body() createMovieDto: CreateMovieDto,
+  //   @QueryRunner() qr: QR,
+  //   @UserId() userId: number,
+  // ) {
+  //   return this.movieService.create(
+  //     createMovieDto,
+  //     userId,
+  //     qr,
+  //   );
+  // }
+  //
+  // @Patch(":id")
+  // @RBAC(Role.admin)
+  // @UseInterceptors(TransactionInterceptor)
+  // patchMovie(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  //   @Body() updateMovieDto: UpdateMovieDto,
+  //   @QueryRunner() qr: QR,
+  // ) {
+  //   return this.movieService.update(id, updateMovieDto, qr);
+  // }
+  //
+  // @Delete(":id")
+  // @RBAC(Role.admin)
+  // @UseInterceptors(TransactionInterceptor)
+  // deleteMovie(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  //   @QueryRunner() qr: QR,
+  // ) {
+  //   return this.movieService.remove(id, qr);
+  // }
+  //
+  // // 좋아요
+  // @Post(":id/like")
+  // @ApiOperation({
+  //   description: "좋아요 API",
+  // })
+  // likeMovie(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe)
+  //   movieId: number,
+  //   @UserId() userId: number,
+  // ) {
+  //   return this.movieService.likeMovie(movieId, userId);
+  // }
+  //
+  // // 싫어요
+  // @Post(":id/dislike")
+  // @ApiOperation({
+  //   description: "싫어요 API",
+  // })
+  // dislikeMovie(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe)
+  //   movieId: number,
+  //   @UserId() userId: number,
+  // ) {
+  //   return this.movieService.dislikeMovie(movieId, userId);
+  // }
 }
