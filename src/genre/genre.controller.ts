138d619c14c1ff4393a97418188e4a34fdfa020c
jjalseu <cjfwns966@gmail.com>
2025-01-30 19:40:51 +0900

    README.md 수정 및 안쓰는 API 주석

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,19 +1,9 @@
 import {
-  Body,
   ClassSerializerInterceptor,
   Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
   UseInterceptors,
 } from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
-import { PositiveIntPipe } from "../shared/pipe/positive-int.pipe";
-import { CreateGenreDto } from "./dto/create-genre.dto";
-import { UpdateGenreDto } from "./dto/update-genre.dto";
 import { GenreService } from "./genre.service";
 
 @Controller("genre")
@@ -25,35 +15,35 @@ export class GenreController {
     private readonly genreService: GenreService,
   ) {}
 
-  @Post()
-  create(@Body() createGenreDto: CreateGenreDto) {
-    return this.genreService.create(createGenreDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.genreService.findAll();
-  }
-
-  @Get(":id")
-  findOne(
-    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-  ) {
-    return this.genreService.findOne(id);
-  }
-
-  @Patch(":id")
-  update(
-    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-    @Body() updateGenreDto: UpdateGenreDto,
-  ) {
-    return this.genreService.update(id, updateGenreDto);
-  }
-
-  @Delete(":id")
-  remove(
-    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-  ) {
-    return this.genreService.remove(id);
-  }
+  // @Post()
+  // create(@Body() createGenreDto: CreateGenreDto) {
+  //   return this.genreService.create(createGenreDto);
+  // }
+  //
+  // @Get()
+  // findAll() {
+  //   return this.genreService.findAll();
+  // }
+  //
+  // @Get(":id")
+  // findOne(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  // ) {
+  //   return this.genreService.findOne(id);
+  // }
+  //
+  // @Patch(":id")
+  // update(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  //   @Body() updateGenreDto: UpdateGenreDto,
+  // ) {
+  //   return this.genreService.update(id, updateGenreDto);
+  // }
+  //
+  // @Delete(":id")
+  // remove(
+  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  // ) {
+  //   return this.genreService.remove(id);
+  // }
 }
