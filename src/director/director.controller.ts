5493dd1370133068e008fbb5b6f63b5b2787ec59
jjalseu <cjfwns966@gmail.com>
2025-01-31 21:13:16 +0900

    movie-feature 주석 해제

--- src/director/director.controller.ts
+++ src/director/director.controller.ts
@@ -1,6 +1,18 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { PositiveIntPipe } from "../shared/pipe/positive-int.pipe";
 import { DirectorService } from "./director.service";
+import { CreateDirectorDto } from "./dto/create-director.dto";
+import { UpdateDirectorDto } from "./dto/update-director.dto";
 
 @Controller("director")
 @ApiBearerAuth()
@@ -10,38 +22,38 @@ export class DirectorController {
     private readonly directorService: DirectorService,
   ) {}
 
-  // @Post()
-  // create(@Body() createDirectorDto: CreateDirectorDto) {
-  //   return this.directorService.create(createDirectorDto);
-  // }
-  //
-  // @Get()
-  // findAll() {
-  //   return this.directorService.findAll();
-  // }
-  //
-  // @Get(":id")
-  // findOne(
-  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-  // ) {
-  //   return this.directorService.findOne(id);
-  // }
-  //
-  // @Patch(":id")
-  // update(
-  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-  //   @Body() updateDirectorDto: UpdateDirectorDto,
-  // ) {
-  //   return this.directorService.update(
-  //     id,
-  //     updateDirectorDto,
-  //   );
-  // }
-  //
-  // @Delete(":id")
-  // remove(
-  //   @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
-  // ) {
-  //   return this.directorService.remove(id);
-  // }
+  @Post()
+  create(@Body() createDirectorDto: CreateDirectorDto) {
+    return this.directorService.create(createDirectorDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.directorService.findAll();
+  }
+
+  @Get(":id")
+  findOne(
+    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  ) {
+    return this.directorService.findOne(id);
+  }
+
+  @Patch(":id")
+  update(
+    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+    @Body() updateDirectorDto: UpdateDirectorDto,
+  ) {
+    return this.directorService.update(
+      id,
+      updateDirectorDto,
+    );
+  }
+
+  @Delete(":id")
+  remove(
+    @Param("id", ParseIntPipe, PositiveIntPipe) id: number,
+  ) {
+    return this.directorService.remove(id);
+  }
 }
